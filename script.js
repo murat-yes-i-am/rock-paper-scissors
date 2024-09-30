e987ae7c63cf6b0b6411d89991e5f34fd6be2102
murat-yesimbietov <yesimbietovm@gmail.com>
2024-09-30 18:10:04 +0300

    feat: add playRound function

--- script.js
+++ script.js
@@ -20,6 +20,25 @@ function getHumanChoice() {
   }
 }
 
+function playRound(humanChoice, computerChoice) {
+  switch (true) {
+    case (humanChoice === ROCK && computerChoice === SCISSORS):
+    case (humanChoice === PAPER && computerChoice === ROCK):
+    case (humanChoice === SCISSORS && computerChoice === PAPER):
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+      break;
+    case (computerChoice === ROCK && humanChoice === SCISSORS):
+    case (computerChoice === PAPER && humanChoice === ROCK):
+    case (computerChoice === SCISSORS && humanChoice === PAPER): 
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+      break;
+    default: 
+      console.log("It's tie");
+  }
+}
+
 let humanScore = 0;
 let computerScore = 0;
 
